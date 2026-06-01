e13427313956626f822c29694635098b276605fd
Sai Krishna <chaitanya458695-gmail@.com>
2026-06-01 14:33:08 +0530

    First Selenium test - opens Google

--- src/test/java/com/automation/AppTest.java
+++ src/test/java/com/automation/AppTest.java
@@ -1,38 +1,16 @@
 package com.automation;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void openGoogle() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Title is: " + driver.getTitle());
+        driver.quit();
     }
 }
