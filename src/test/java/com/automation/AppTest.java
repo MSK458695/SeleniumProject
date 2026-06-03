2b92b74b5c3c89738fc0a773d5190daf140a627e
Sai Krishna <chaitanya458695-gmail@.com>
2026-06-03 09:17:08 +0530

    Day 2 - Assertions and wrong password test

--- src/test/java/com/automation/AppTest.java
+++ src/test/java/com/automation/AppTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
 
@@ -16,23 +17,54 @@ public class AppTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/");
         
-        // Wait up to 10 seconds for username field to appear
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         
-        // Find username field and type
+        // Login
         WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
         username.sendKeys("Admin");
         
-        // Find password field and type
         WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("admin123");
         
-        // Find login button and click
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
         loginButton.click();
         
-        // Print page title after login
-        System.out.println("Page title is: " + driver.getTitle());
+        // Wait for dashboard to load
+        wait.until(ExpectedConditions.titleContains("OrangeHRM"));
+        
+        // ASSERTION - verify login was successful
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "OrangeHRM";
+        
+        Assert.assertEquals(actualTitle, expectedTitle);
+        System.out.println("Login successful. Title verified: " + actualTitle);
+        
+        driver.quit();
+    }
+    
+    @Test
+    public void wrongPasswordTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Login with wrong password
+        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        username.sendKeys("Admin");
+        
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("wrongpassword");
+        
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+        
+        // Verify error message appears
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+            By.cssSelector(".oxd-alert-content-text")));
+        
+        Assert.assertTrue(errorMessage.isDisplayed());
+        System.out.println("Error message verified: " + errorMessage.getText());
         
         driver.quit();
     }
